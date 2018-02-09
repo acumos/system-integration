6cd10c2ab2d9d955b59c803952202d750f71a191
unknown <SS00492019@TechMahindra.com>
2018-02-09 16:29:51 +0530

    OnboaridngScript
    Change-Id: Ie14c997bf409692aec1a028fcc5f9421af5db154
    Signed-off-by: unknown <SS00492019@TechMahindra.com>

---
+++ bootstrap/properties.sh
@@ -0,0 +1,12 @@
+# Don't run this script directly; it's used by bootstrap.sh
+# edit this to add credentials/URL for your specific Acumos instance 
+
+# Acumos user credentials for bootstrapped models
+USERNAME=sohil
+PASSWORD=Test@1234
+
+# Acumos onboarding URLS
+AUTHURL=http://cognita-dev1-vm01-core:8090/onboarding-app/v2/auth
+PUSHURL=http://cognita-dev1-vm01-core:8090/onboarding-app/v2/models
+      
+
