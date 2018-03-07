da3c160dee5197e683afb64083d0ecfd17ee69f4
Bryan Sullivan bs3131 <bs3131@att.com>
2018-03-07 15:24:00 -0800

    WIP: Baseline of all-in-one deploy process
    Change-Id: I450b79a6768dcd17fcf3b1f46ae82fc367911743
    IssueID: ACUMOS-231
    Signed-off-by: Bryan Sullivan <bryan.sullivan@att.com>
    Signed-off-by: Lott, Christopher (cl778h) <cl778h@att.com>

---
+++ AIO/clean.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# ===============LICENSE_START=======================================================
+# Acumos Apache-2.0
+# ===================================================================================
+# Copyright (C) 2018 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+# ===================================================================================
+# This Acumos software file is distributed by AT&T and Tech Mahindra
+# under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# This file is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============LICENSE_END=========================================================
+#
+# What this is: cleanup script for an All-in-One deployment of the Acumos platform.
+# Prerequisites:
+# - Acumos AIO installed per oneclick_deploy.sh
+# - If the docker-compose console is still running, showing the logs of the
+#   containers, ctrl-c to stop it and wait will all services are stopped.
+# Usage:
+# $ sudo clean.sh
+#
+
+source acumos-env.sh
+
+echo "Stop the running Acumos component containers"
+docker-compose down
+docker-compose rm -v
+
+echo "Stop the Nexus container"
+docker stop nexus
+docker rm -v nexus
+
+echo "Remove Acumos databases and users"
+# TODO: support centos also
+mysql --user=root --password=$MARIADB_PASSWORD -e "DROP DATABASE acumos; DROP DATABASE acumos_comment; DROP USER 'acumos_opr'@'localhost'; DROP USER 'acumos_opr'@'%'"
+
+echo "Remove mariadb-server"
+apt-get remove mariadb-server-10.2 -y
+
+echo "Remove all mysql data"
+# TODO: remove this workaround used in early testing
+rm -rf /var/lib/mysql
+
+echo "You should now be able to repeat the install via oneclick_deploy.sh"
---
+++ README.md
@@ -0,0 +1,22 @@
+# Acumos System Integration
+
+This repository holds installation and deployment scripts for the Acumos system.
+
+Please see the documentation in the "docs" folder.
+
+## All In One (AIO)
+
+The AIO subdirectory holds scripts to build an all-in-one instance of Acumos, with the database,
+Nexus repositories and docker containers all running on a single virtual machine.
+
+## License
+
+Copyright (C) 2017 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+Acumos is distributed by AT&T and Tech Mahindra under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+express or implied.  See the License for the specific language governing permissions and limitations 
+under the License.
