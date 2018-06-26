2a9d19d48942d65ccf9f05285d69eb3aef95628d
GUPTA <pg0057291@techmahindra.com>
2018-06-26 18:28:58 -0400

    Deploy of Acumos platform under Azure-k8s
    Added Azure client and filebeat
    TODO:Need to add remaining components
    
    Change-Id: I19a4346e79b60210b9fbfa5b52116ada78963fdd
    Issue-ID: ACUMOS-1043
    Signed-off-by: PARICHAY GUPTA <pg0057291@techmahindra.com>
    Signed-off-by: GUPTA <pg0057291@techmahindra.com>

---
+++ acumosk8s-public-cloud/azure/acumos-kubectl-env.sh
@@ -0,0 +1,127 @@
+# ===============LICENSE_START=======================================================
+# Acumos Apache-2.0
+# ===================================================================================
+# Copyright (C) 2017-2018 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+# ===================================================================================
+# This Acumos software file is distributed by AT&T and Tech Mahindra
+# under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# This file is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============LICENSE_END=========================================================
+
+# Registry
+
+export NEXUS3_RELEASE_REGISTRY_LF=nexus3.acumos.org:10002
+export NEXUS3_REGISTRY_NAME=nexus3.acumos.org
+
+
+#Ports
+export ACUMOS_MARIADB_PORT=3306
+export ACUMOS_MARIADB_ROOT_PASSWORD=XXXXX
+export ACUMOS_PORTAL_FE_PORT=8085
+export ACUMOS_CMS_PORT=9080
+export ACUMOS_PORTAL_BE_PORT=8083
+export ACUMOS_PORTAL_DS_COMPOSITION_PORT=8088
+export ACUMOS_COMMON_DATA_SVC_PORT=8000
+export ACUMOS_ONBOARDING_APP_PORT=8090
+export ACUMOS_FEDERATION_GATEWAY_PORT=9084
+export ACUMOS_PLATON_PORT=9083
+export ACUMOS_AZURE_CLIENT_PORT=9081
+export ACUMOS_KONG_POSTGRES_PORT=5432
+export ACUMOS_KONG_ADMIN_PORT=8001
+export ACUMOS_KONG_PROXY_PORT=8000
+export ACUMOS_KONG_SSL_PORT=8443
+export ACUMOS_KONG_ADM_SSL_PORT=8444
+export ACUMOS_NEXUS_PORT=8001
+export ACUMOS_NEXUS_ENDPOINT_PORT=8081
+export ACUMOS_PROXY_PORT=3128
+export ACUMOS_DOCKER_PORT=2375
+export ACUMOS_VALIDATION_PORT=9603
+export ACUMOS_KIBANA_PORT=5601
+export ACUMOS_DEPLOYED_SOLUTION_PORT=3330
+export ACUMOS_DATA_BROKER_PORT=8556
+export ACUMOS_DATA_BROKER_INTERNAL_PORT=8080
+export ACUMOS_DOCKER_PLATFORM_PORT=8002
+export ACUMOS_PROBE_PORT=5006
+export ACUMOS_FILEBEAT_PORT=8099
+
+
+# Images
+export ACUMOS_ONBOARDING_IMAGE=onboarding-app:1.27.0
+export ACUMOS_PORTAL_BE_IMAGE=acumos-portal-be:1.15.33
+export ACUMOS_PORTAL_FE_IMAGE=acumos-portal-fe:1.15.33
+export ACUMOS_CMS_IMAGE=acumos-cms-docker:1.3.4
+export ACUMOS_DESIGN_STUDIO_IMAGE=ds-compositionengine:0.0.30
+export ACUMOS_DATA_BROKER_IMAGE=databroker-zipbroker:0.0.1
+export ACUMOS_CSV_DATA_BROKER_IMAGE=csvdatabroker:0.0.1
+export ACUMOS_CDS_IMAGE=common-dataservice:1.14.4
+export ACUMOS_FEDERATION_IMAGE=federation-gateway:1.1.3
+export ACUMOS_FILEBEAT_IMAGE=acumos-filebeat:1.0.0
+export ACUMOS_METRICBEAT_IMAGE=acumos-metricbeat:1.1.0
+export ACUMOS_AZURE_CLIENT_IMAGE=acumos-azure-client:1.2.4
+export ACUMOS_VALIDATION_CLIENT=validation-client:1.2
+export ACUMOS_VALIDATION_ENGINE=validation-engine:1.2.2
+export ACUMOS_VALIDATION_MIDDLEWARE=validation-middleware:1.2
+export ACUMOS_OPENSTACK=openstack-client:1.41.0
+export ACUMOS_PROTO_VIEWER=acumos_proto_viewer:1.4.1
+export ACUMOS_BLUE_PRINT_ORCHESTRATOR=blueprint-orchestrator:1.0.13
+
+
+#Nexus
+export ACUMOS_DOCKER_HOST=acumos-docker-service
+export ACUMOS_NEXUS_SERVICE=acumos-nexus-service
+export ACUMOS_NEXUS_USERNAME=acumos-k8-user-rw
+export ACUMOS_NEXUS_PASSWORD=xxxxxxx
+export ACUMOS_PROXY=acumos-proxy
+
+#Azure client
+export ACUMOS_DEPLOYED_VM_USER=dockerUser
+export ACUMOS_DEPLOYED_VM_PASSWORD=xxxxxxxx
+export ACUMOS_RW_USER_PASSWORD=xxxxxxxxx
+export ACUMOS_RW_USER_NAME=xxxxxx
+export ACUMOS_AZURE_CONTAINER_NAME_PREFIX=acumos-e6e
+export ACUMOS_AZURE_SUBNET=xxxxxxxxxx-vsubnet
+export ACUMOS_AZURE_VNET=xxxxxxxx-vnet
+export ACUMOS_AZURE_NETWORK_GROUP_NAME=E6E-NSG
+
+#Kong and Database
+export ACUMOS_CDS_DB=CDS
+export ACUMOS_CDS_USER=ccds_client
+export ACUMOS_CDS_PASSWORD=xxxxxx
+export ACUMOS_MARIADB_USER=CDS_USER
+export ACUMOS_MARIADB_PASSWORD=xxxxx
+export ACUMOS_CMS_DB=acumos_CMS
+export ACUMOS_CMS_USER=CMS_USER
+export ACUMOS_CMS_PASSWORD=xxxxxxx
+export ACUMOS_KONG_POSTGRES_USER=kong
+export ACUMOS_KONG_POSTGRES_PASSWORD=xxxx
+export ACUMOS_KONG_POSTGRES_DB=kong
+export ACUMOS_KONG_PG_PASSWORD=xxxx
+
+#Namespace and PVC
+export ACUMOS_NAMESPACE=acumos-ns01
+export ACUMOS_MARIADB_PVC_STORAGE=1Gi
+export ACUMOS_PVC_STORAGE=1Gi
+export ACUMOS_NEXUS_PVC_STORAGE=5Gi
+
+export ACUMOS_QANDA_URL=stackoverflow.com
+export ACUMOS_MAILJET_ID=user@domainname
+export ACUMOS_MAILJET_API_KEY=xxxxxxxxxxxxxxx
+export ACUMOS_MAILJET_SECRET_KEY=xxxxxxxxxxxxx
+export ACUMOS_TRUST_STORE_PWD=xxxxxx
+export ACUMOS_KEY_STORE_PWD=xxxxxx
+export ACUMOS_DOCKER_REGISTRY_EMAIL=user@domainname
+export ACUMOS_LOGSTASH_HOST=logcollector
+
+
+#1: Env file 
+#2: YAML File
+#3: create/delete
+envsubst < $2 | kubectl $1 -f -
