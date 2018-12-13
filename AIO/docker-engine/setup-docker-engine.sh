07bc92cc46c3e53930243271168f626e71a7dfde
Bryan Sullivan <bryan.sullivan@att.com>
2018-12-13 14:28:18 -0800

    Oneclick deployment of Acumos on OpenShift
    Issue-ID: ACUMOS-2301
    - include changes for ACUMOS-2150
    - add setup_openshift.sh and setup_openshift_client.sh
    - reintroduce docker-service via docker-dind
    - Connect kong to kong-database directly
    - Allow user to set target namespace
    - Simplify install reset
    - Add Centos-specific prereqs and cleanup
    - Remove host installation of docker for k8s/OpenShift
    - Add option for generic k8s or OpenShift installs
    - Add ELK option for docker-compose to start/stop
    - use "oc" in place of "kubectl" for OpenShift
    - Improve method of determining primary IP address
    - add support for Ubuntu 18.04
    - for Centos, use docker config from /root
    - replace use of "~" with $HOME
    - add K8S_DIST to acumos-env.sh
    - refactor to separate core components from non-core
    - migrate host-installed components (e.g. mariadb) to docker
    - build local images for customization
    - store persistent data in PV/PVC under k8s
    - create resources (e.g. PV, PVC) using ACUMOS_NAMESPACE
    - address OpenShift-specific constraints e.g. for security
    - update other tools to be compatible with the changes
    - align designs where possible across docker, k8s-generic, k8s-openshift
    - improve method of determining deployment env so user
      does not have to specify
    - update patched federation templates to support redeployment
    
    Change-Id: I1d4ba6863859bb83c3bbe59e8a9ec114ec7f5f5b
    Signed-off-by: Bryan Sullivan <bryan.sullivan@att.com>

---
+++ AIO/docker-engine/setup-docker-engine.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+# ===============LICENSE_START=======================================================
+# Acumos Apache-2.0
+# ===================================================================================
+# Copyright (C) 2017-2018 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+# ===================================================================================
+# This Acumos software file is distributed by AT&T and Tech Mahindra
+# under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# This file is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============LICENSE_END=========================================================
+#
+# What this is: Deployment script for the docker-engine and API server as a
+# dependency of the Acumos platform.
+#
+# Prerequisites:
+# - Ubuntu Xenial or Centos 7 server
+# - acumos-env.sh customized for this platform, as by oneclick_deploy.sh
+#
+# Usage: intended to be called from oneclick_deploy.sh
+# - cd docker-engine; bash setup-docker-engine.sh
+#
+
+setup_prereqs() {
+  if [[ "$ACUMOS_HOST_OS" == "ubuntu" ]]; then
+    if [[ "$DEPLOYED_UNDER" = "docker" ]]; then
+      case "$ACUMOS_HOST_OS_VER" in
+        "16.04")
+          log "Install docker-ce if needed"
+          dce=$(/usr/bin/dpkg-query --show --showformat='${db:Status-Status}\n' 'docker-ce')
+          if [[ $dce != "installed" ]]; then
+            echo; echo "prereqs.sh: ($(date)) Install latest docker-ce"
+            # Per https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/
+            sudo apt-get purge -y docker-ce docker docker-engine docker.io
+            sudo apt-get update
+            sudo apt-get install -y \
+              apt-transport-https \
+              ca-certificates \
+              curl \
+              software-properties-common
+            curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+            sudo add-apt-repository "deb [arch=amd64] \
+              https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+            sudo apt-get update
+            sudo apt-get install -y docker-ce=17.03.3~ce-0~ubuntu-xenial
+          fi
+          ;;
+        "18.04")
+          log "Install docker.io if needed"
+          dio=$(/usr/bin/dpkg-query --show --showformat='${db:Status-Status}\n' 'docker.io')
+          if [[ $dio != "installed" ]]; then
+            sudo apt-get purge -y docker docker-engine docker-ce docker-ce-cli
+            sudo apt-get update
+            sudo apt-get install -y docker.io=17.12.1-0ubuntu1
+            sudo systemctl enable docker.service
+          fi
+          ;;
+        *)
+          fail "Unsupported Ubuntu version ($ACUMOS_HOST_OS_VER)"
+      esac
+    fi
+  fi
+}
+
+setup() {
+  if [[ "$DEPLOYED_UNDER" = "docker" ]]; then
+     log "Enable docker API on the AIO install host"
+     if [[ $(grep -c "\-H tcp://0.0.0.0:$ACUMOS_DOCKER_API_PORT" /lib/systemd/system/docker.service) -eq 0 ]]; then
+       sudo sed -i -- "s~ExecStart=/usr/bin/dockerd -H fd://~ExecStart=/usr/bin/dockerd -H fd:// -H tcp://0.0.0.0:$ACUMOS_DOCKER_API_PORT~" /lib/systemd/system/docker.service
+       # Add another variant of this config setting
+       # TODO: find a general solution
+       sudo sed -i -- "s~ExecStart=/usr/bin/dockerd -H unix://~ExecStart=/usr/bin/dockerd -H unix:// -H tcp://0.0.0.0:$ACUMOS_DOCKER_API_PORT~" /lib/systemd/system/docker.service
+     fi
+
+     log "Block host-external access to docker API"
+     if [[ $(sudo iptables -S | grep -c '172.0.0.0/8 .* 2375') -eq 0 ]]; then
+       sudo iptables -A INPUT -p tcp --dport 2375 ! -s 172.0.0.0/8 -j DROP
+     fi
+     if [[ $(sudo iptables -S | grep -c '127.0.0.1/32 .* 2375') -eq 0 ]]; then
+       sudo iptables -I INPUT -s localhost -p tcp -m tcp --dport 2375 -j ACCEPT
+     fi
+     if [[ $(sudo iptables -S | grep -c "$ACUMOS_HOST/32 .* 2375") -eq 0 ]]; then
+       sudo iptables -I INPUT -s $ACUMOS_HOST -p tcp -m tcp --dport 2375 -j ACCEPT
+     fi
+
+     log "Enable non-secure docker repositories"
+     cat << EOF | sudo tee /etc/docker/daemon.json
+{
+   "insecure-registries": [
+     "$ACUMOS_NEXUS_HOST:$ACUMOS_DOCKER_MODEL_PORT"
+   ],
+   "disable-legacy-registry": true
+}
+EOF
+     log "Restart the docker service to apply the changes"
+     # NOTE: the need to do this is why docker-dind is required for OpenShift;
+     # restarting the docker service kills all docker-based services in centos
+     # and they are not restarted - thus this kills the OpenShift stack
+     sudo systemctl daemon-reload
+     sudo service docker restart
+     log "Verify docker API is accessible"
+     url=http://$ACUMOS_DOCKER_API_HOST:$ACUMOS_DOCKER_API_PORT
+  else
+    log "Stop any existing k8s based components for docker-service"
+    stop_service deploy/docker-service.yaml
+    stop_deployment deploy/docker-deployment.yaml
+
+    log "Setup the $ACUMOS_NAMESPACE-docker-volume PV"
+    bash $AIO_ROOT/setup-pv.sh setup pv docker-volume \
+      $PV_SIZE_DOCKER_VOLUME "$USER:$USER"
+    log "Setup the $ACUMOS_NAMESPACE-docker-volume PVC"
+    bash $AIO_ROOT/setup-pv.sh setup pvc docker-volume \
+      $PV_SIZE_DOCKER_VOLUME
+
+    log "Deploy the k8s based components for docker-service"
+    mkdir -p deploy
+    cp -r kubernetes/* deploy/.
+    replace_env deploy "ACUMOS_DOCKER_API_PORT ACUMOS_DOMAIN ACUMOS_HOST \
+      ACUMOS_NAMESPACE ACUMOS_NEXUS_HOST HTTPS_PROXY HTTP_PROXY \
+      ACUMOS_DOCKER_MODEL_PORT"
+
+    start_service deploy/docker-service.yaml
+    start_deployment deploy/docker-deployment.yaml
+    wait_running docker-service
+    ip=$(kubectl get svc -n acumos docker-service | awk '/docker-service/{print $3}')
+    url=http://$ip:$ACUMOS_DOCKER_API_PORT
+  fi
+
+  log "Wait for docker API to be ready at $url"
+  while ! curl $url; do
+    log "docker API not ready ... waiting 10 seconds"
+    sleep 10
+  done
+}
+
+source $AIO_ROOT/acumos-env.sh
+source $AIO_ROOT/utils.sh
+setup_prereqs
+setup
---
+++ AIO/setup-pv.sh
@@ -0,0 +1,151 @@
+#!/bin/bash
+# ===============LICENSE_START=======================================================
+# Acumos Apache-2.0
+# ===================================================================================
+# Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+# ===================================================================================
+# This Acumos software file is distributed by AT&T
+# under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# This file is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============LICENSE_END=========================================================
+#
+#. What this is: script to setup host-mapped PVs under kubernetes or docker
+#.
+#. Prerequisites:
+#. - acumos-env.sh script prepared through oneclick_deploy.sh or manually, to
+#.   set install options (e.g. docker/k8s)
+#. - for k8s, k8s cluster installed using generic k8s or OpenShift
+#.
+#. Usage: on the k8s master
+#. $ bash setup-pv.sh <action> <what> <namespace> <pv> <size> <owner>
+#.   action: setup|clean
+#.   what: pv|pvc (setup a PV or a PVC)
+#.   namespace: for k8s, namespace to create the PVC under
+#.   size: size in Gi or Mi (e.g. 2Gi)
+#.   owner: host account to set as owner of the host folder
+#.
+#. Note if a PV exists, the PV will be deleted but the host foolder will be
+#. preserved. If needed, manually delete data before redeploying.
+
+function setup_pv() {
+  # Per https://kubernetes.io/docs/tasks/configure-pod-container/configure-persistent-volume-storage/
+  mkdir -p /tmp/$ACUMOS_NAMESPACE/yaml
+  if [[ ! -e /var/$ACUMOS_NAMESPACE ]]; then
+    log "Creating /var/$ACUMOS_NAMESPACE as PV root folder"
+    sudo mkdir /var/$ACUMOS_NAMESPACE
+    # Have to set user and group to allow pod access to PVs
+    sudo chown $USER:$USER /var/$ACUMOS_NAMESPACE
+  fi
+
+  if [[ "$DEPLOYED_UNDER" = "docker" ]]; then
+    log "Creating folder for host-path docker volume $pv"
+    path="/var/$ACUMOS_NAMESPACE/$pv"
+    if [[ ! -e $path ]]; then
+      mkdir -p $path
+      # TODO: remove/relax this workaround
+      # Required for various components to be able to write to the PVs
+      chmod 777 $path
+    fi
+  else
+    log "Creating kubernetes PV pv-$ACUMOS_NAMESPACE-$pv"
+    path="/var/$ACUMOS_NAMESPACE/$pv"
+    if [[ ! -e $path ]]; then
+      mkdir $path
+      sudo chown $owner $path
+      # TODO: remove/relax this workaround
+      # TODO: some writes fail in Centos without this
+      chmod 777 $path
+    fi
+    cat <<EOF >/tmp/$ACUMOS_NAMESPACE/yaml/pv-$ACUMOS_NAMESPACE-$pv.yaml
+kind: PersistentVolume
+apiVersion: v1
+metadata:
+  name: pv-$ACUMOS_NAMESPACE-$pv
+spec:
+  storageClassName: $ACUMOS_NAMESPACE
+  capacity:
+    storage: $size
+  accessModes:
+    - ReadWriteOnce
+  persistentVolumeReclaimPolicy: Retain
+  hostPath:
+    path: "$path"
+EOF
+
+    $k8s_cmd create -f /tmp/$ACUMOS_NAMESPACE/yaml/pv-$ACUMOS_NAMESPACE-$pv.yaml
+    $k8s_cmd get pv pv-$ACUMOS_NAMESPACE-$pv
+  fi
+}
+
+function setup_pvc() {
+  log "Creating PVC pvc-$ACUMOS_NAMESPACE-$pv"
+  # Add volumeName: to ensure the PVC selects a specific volume as data
+  # may be pre-configured there
+  cat <<EOF >/tmp/$ACUMOS_NAMESPACE/yaml/pvc-$ACUMOS_NAMESPACE-$pv.yaml
+kind: PersistentVolumeClaim
+apiVersion: v1
+metadata:
+  name: pvc-$ACUMOS_NAMESPACE-$pv
+spec:
+  storageClassName: $ACUMOS_NAMESPACE
+  accessModes:
+    - ReadWriteOnce
+  resources:
+    requests:
+      storage: $size
+  volumeName: "pv-$ACUMOS_NAMESPACE-$pv"
+EOF
+
+  $k8s_cmd create -n $ACUMOS_NAMESPACE -f /tmp/$ACUMOS_NAMESPACE/yaml/pvc-$ACUMOS_NAMESPACE-$pv.yaml
+  $k8s_cmd get pvc -n $ACUMOS_NAMESPACE pvc-$ACUMOS_NAMESPACE-$pv
+}
+
+function clean() {
+  if [[ "$DEPLOYED_UNDER" = "k8s" ]]; then
+    log "Deleting $what-$ACUMOS_NAMESPACE-$pv"
+    if [[ "$what" == "pvc" ]]; then
+      $k8s_cmd delete pvc -n $ACUMOS_NAMESPACE pvc-$ACUMOS_NAMESPACE-$pv
+      wait_until_fail "$k8s_cmd get pvc -n $ACUMOS_NAMESPACE pvc-$ACUMOS_NAMESPACE-$pv"
+      rm /tmp/$ACUMOS_NAMESPACE/yaml/pvc-$ACUMOS_NAMESPACE-$pv.yaml
+    else
+      $k8s_cmd delete pv pv-$ACUMOS_NAMESPACE-$pv
+      wait_until_fail "$k8s_cmd get pv pv-$ACUMOS_NAMESPACE-$pv"
+      rm /tmp/$ACUMOS_NAMESPACE/yaml/pv-$ACUMOS_NAMESPACE-$pv.yaml
+    fi
+  fi
+  if [[ "$ACUMOS_CDS_PREVIOUS_VERSION" == "" ]]; then
+    if [[ -e /var/$ACUMOS_NAMESPACE/$pv ]]; then
+      log "Deleting host folder /var/$ACUMOS_NAMESPACE/$pv"
+      sudo rm -rf /var/$ACUMOS_NAMESPACE/$pv
+    fi
+  else
+    log "Not deleting /var/$ACUMOS_NAMESPACE/$pv: platform is being redeployed"
+  fi
+}
+
+source $AIO_ROOT/acumos-env.sh
+source $AIO_ROOT/utils.sh
+
+action=$1
+what=$2
+pv=$3
+size=$4
+owner=$5
+
+if [[ "$action" == "clean" ]]; then
+  clean
+else
+  if [[ "$what" == "pv" ]]; then
+    setup_pv
+  else
+    setup_pvc
+  fi
+fi
