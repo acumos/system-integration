136ba818c5dfc841e4a97d55a6c42846a9b7c2bb
Bryan Sullivan <bryan.sullivan@att.com>
2018-12-13 14:28:18 -0800

    Oneclick deployment of Acumos on OpenShift
    Issue-ID: ACUMOS-2301
    - include changes for ACUMOS-2150
    - add setup_openshift.sh and setup_openshift_client.sh
    - reintroduce docker-service via docker-dind
    - Connect kong to kong-database directly
    - Allow user to set target namespace
    - Simplify install reset
    - Add Centos-specific prereqs and cleanup
    - Remove host installation of docker for k8s/OpenShift
    - Add option for generic k8s or OpenShift installs
    - Add ELK option for docker-compose to start/stop
    - use "oc" in place of "kubectl" for OpenShift
    - Improve method of determining primary IP address
    - add support for Ubuntu 18.04
    - for Centos, use docker config from /root
    - replace use of "~" with $HOME
    - add K8S_DIST to acumos-env.sh
    - refactor to separate core components from non-core
    - migrate host-installed components (e.g. mariadb) to docker
    - build local images for customization
    - store persistent data in PV/PVC under k8s
    - create resources (e.g. PV, PVC) using ACUMOS_NAMESPACE
    - address OpenShift-specific constraints e.g. for security
    - update other tools to be compatible with the changes
    - align designs where possible across docker, k8s-generic, k8s-openshift
    
    Change-Id: I1d4ba6863859bb83c3bbe59e8a9ec114ec7f5f5b
    Signed-off-by: Bryan Sullivan <bryan.sullivan@att.com>

---
+++ AIO/docker-proxy/setup-docker-proxy.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+# ===============LICENSE_START=======================================================
+# Acumos Apache-2.0
+# ===================================================================================
+# Copyright (C) 2017-2018 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+# ===================================================================================
+# This Acumos software file is distributed by AT&T and Tech Mahindra
+# under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# This file is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============LICENSE_END=========================================================
+#
+# What this is: Deployment script for Acumos docker-proxy based upon nginx per
+# https://docs.docker.com/registry/recipes/nginx/#setting-things-up
+#
+# Prerequisites:
+# - Ubuntu Xenial or Centos 7 server
+# - acumos-env.sh customized for this platform, as by oneclick_deploy.sh
+#
+# Usage: intended to be called from oneclick_deploy.sh
+# - cd docker-proxy; bash setup-docker-proxy.sh; cd ..
+#
+# See https://docs.acumos.org/en/latest/submodules/kubernetes-client/docs/deploy-in-private-k8s.html#operations-user-guide
+# for more information, e.g. on use of this script for manually-installed
+# Acumos platforms
+
+setup_prereqs() {
+  log "Install latest docker-compose"
+  if [[ "$ACUMOS_HOST_OS" == "ubuntu" ]]; then
+    wait_dpkg
+    sudo apt-get install -y docker-compose
+  else
+    sudo yum install -y docker-compose
+  fi
+}
+
+setup() {
+  log "Update docker-proxy config for building the docker image"
+  ACUMOS_DOCKER_PROXY_AUTH=$(echo -n "$ACUMOS_RO_USER:$ACUMOS_RO_USER_PASSWORD" | base64)
+  sed -i -- "s~<ACUMOS_DOCKER_PROXY_AUTH>~$ACUMOS_DOCKER_PROXY_AUTH~g" auth/nginx.conf
+  sed -i -- "s~<ACUMOS_NEXUS_HOST>~$ACUMOS_NEXUS_HOST~g" auth/nginx.conf
+  sed -i -- "s~<ACUMOS_DOCKER_MODEL_PORT>~$ACUMOS_DOCKER_MODEL_PORT~g" auth/nginx.conf
+  sed -i -- "s~<ACUMOS_DOCKER_PROXY_PORT>~$ACUMOS_DOCKER_PROXY_PORT~g" auth/nginx.conf
+  sed -i -- "s~<ACUMOS_DOCKER_PROXY_HOST>~$ACUMOS_DOCKER_PROXY_HOST~g" auth/nginx.conf
+
+  log "Generate auth/nginx.htpasswd for the docker image"
+  sudo docker run --rm --entrypoint htpasswd registry:2 \
+    -Bbn $ACUMOS_DOCKER_PROXY_USERNAME $ACUMOS_DOCKER_PROXY_PASSWORD > auth/nginx.htpasswd
+
+  log "Copy the Acumos server cert and key to auth/ for the docker image"
+  cp /var/$ACUMOS_NAMESPACE/certs/acumos.crt auth/domain.crt
+  cp /var/$ACUMOS_NAMESPACE/certs/acumos.key auth/domain.key
+
+  log "Build the local acumos-docker-proxy image"
+  sudo docker build -t acumos-docker-proxy .
+
+  if [[ "$DEPLOYED_UNDER" == "docker" ]]; then
+    # --build will restart any existing container with any new configuration
+    sudo bash docker-compose.sh $AIO_ROOT up -d --build --force-recreate
+  else
+    log "Stop any existing k8s based components for docker-proxy"
+    stop_service deploy/docker-proxy-service.yaml
+    stop_deployment deploy/docker-deployment.yaml
+
+    log "Deploy the k8s based components for docker-proxy"
+    mkdir -p deploy
+    cp -r kubernetes/* deploy/.
+    replace_env deploy "ACUMOS_DOCKER_API_PORT ACUMOS_DOMAIN ACUMOS_HOST \
+      ACUMOS_NAMESPACE ACUMOS_NEXUS_HOST HTTPS_PROXY HTTP_PROXY \
+      ACUMOS_DOCKER_MODEL_PORT"
+
+    start_service deploy/docker-proxy-service.yaml
+    start_deployment deploy/docker-proxy-deployment.yaml
+  fi
+
+  wait_running docker-proxy
+}
+
+source $AIO_ROOT/acumos-env.sh
+source $AIO_ROOT/utils.sh
+setup_prereqs
+setup
