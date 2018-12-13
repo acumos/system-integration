e198488df492d7502ba66c3fa14e61e23d59dbb1
Bryan Sullivan <bryan.sullivan@att.com>
2018-12-13 14:28:18 -0800

    Oneclick deployment of Acumos on OpenShift
    Issue-ID: ACUMOS-2301
    - include changes for ACUMOS-2150
    - add setup_openshift.sh and setup_openshift_client.sh
    - reintroduce docker-service via docker-dind
    - Connect kong to kong-database directly
    - Allow user to set target namespace
    - Simplify install reset
    - Add Centos-specific prereqs and cleanup
    - Remove host installation of docker for k8s/OpenShift
    - Add option for generic k8s or OpenShift installs
    - Add ELK option for docker-compose to start/stop
    - use "oc" in place of "kubectl" for OpenShift
    - Improve method of determining primary IP address
    - add support for Ubuntu 18.04
    - for Centos, use docker config from /root
    - replace use of "~" with $HOME
    - add K8S_DIST to acumos-env.sh
    - refactor to separate core components from non-core
    - migrate host-installed components (e.g. mariadb) to docker
    - build local images for customization
    - store persistent data in PV/PVC under k8s
    - create resources (e.g. PV, PVC) using ACUMOS_NAMESPACE
    - address OpenShift-specific constraints e.g. for security
    - support Linux, Mac, Windows for OpenShift-CLI client
    - update other tools to be compatible with the changes
    - align designs where possible across docker, k8s-generic, k8s-openshift
    - improve method of determining deployment env so user
      does not have to specify
    - update patched federation templates to support redeployment
    
    Change-Id: I1d4ba6863859bb83c3bbe59e8a9ec114ec7f5f5b
    Signed-off-by: Bryan Sullivan <bryan.sullivan@att.com>

---
+++ AIO/setup-acumosdb.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# ===============LICENSE_START=======================================================
+# Acumos Apache-2.0
+# ===================================================================================
+# Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+# ===================================================================================
+# This Acumos software file is distributed by AT&T
+# under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# This file is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============LICENSE_END=========================================================
+#
+#. What this is: script to setup Acumos database
+#.
+#. Prerequisites:
+#. - acumos-env.sh script prepared through oneclick_deploy.sh or manually, to
+#.   set install options (e.g. docker/k8s)
+#.
+#. Usage: intended to be called directly from oneclick_deploy.sh
+#.
+
+function setup() {
+  trap 'fail' ERR
+  if [[ "$ACUMOS_MARIADB" != "on-host" ]]; then
+    server="-h $ACUMOS_HOST -P $ACUMOS_MARIADB_NODEPORT"
+  fi
+
+  if [[ "$ACUMOS_CDS_PREVIOUS_VERSION" == "" ]]; then
+    log "Setting up Acumos database for CDS version $ACUMOS_CDS_VERSION"
+    # NOTE: user and default database was created in the process of server creation
+    log "Setup user for access to database $ACUMOS_CDS_DB"
+    mysql $server --user=root --password=$ACUMOS_MARIADB_PASSWORD \
+      -e "USE $ACUMOS_CDS_DB; GRANT ALL PRIVILEGES ON $ACUMOS_CDS_DB.* TO \"$ACUMOS_MARIADB_USER\"@'%' IDENTIFIED BY \"$ACUMOS_MARIADB_USER_PASSWORD\";"
+
+    log "Retrieve and customize database script for CDS version $ACUMOS_CDS_VERSION"
+    if [[ $(ls cmn-data-svc-ddl-dml-mysql*) != "" ]]; then rm cmn-data-svc-ddl-dml-mysql*; fi
+    wget https://raw.githubusercontent.com/acumos/common-dataservice/master/cmn-data-svc-server/db-scripts/cmn-data-svc-ddl-dml-mysql-$ACUMOS_CDS_VERSION.sql
+    sed -i -- "1s/^/use $ACUMOS_CDS_DB;\n/" cmn-data-svc-ddl-dml-mysql-$ACUMOS_CDS_VERSION.sql
+    mysql $server --user=$ACUMOS_MARIADB_USER --password=$ACUMOS_MARIADB_USER_PASSWORD < cmn-data-svc-ddl-dml-mysql-$ACUMOS_CDS_VERSION.sql
+
+    log "Setup database 'acumos_cms'"
+    mysql $server --user=root --password=$ACUMOS_MARIADB_PASSWORD -e "CREATE DATABASE acumos_cms; USE acumos_cms; GRANT ALL PRIVILEGES ON acumos_cms.* TO \"$ACUMOS_MARIADB_USER\"@'%' IDENTIFIED BY \"$ACUMOS_MARIADB_USER_PASSWORD\";"
+  elif [[ "$ACUMOS_CDS_PREVIOUS_VERSION" != "$ACUMOS_CDS_VERSION" ]]; then
+    log "Upgrading database from CDS version $ACUMOS_CDS_PREVIOUS_VERSION to $ACUMOS_CDS_VERSION"
+    upgrade="cds-mysql-upgrade-${ACUMOS_CDS_PREVIOUS_VERSION}-to-${ACUMOS_CDS_VERSION}.sql"
+    if [[ $(ls ${upgrade}*) != "" ]]; then rm ${upgrade}*; fi
+    if [[ $(wget https://raw.githubusercontent.com/acumos/common-dataservice/master/cmn-data-svc-server/db-scripts/$upgrade) ]]; then
+      sed -i -- "1s/^/use $ACUMOS_CDS_DB;\n/" $upgrade
+      mysql $server --user=$ACUMOS_MARIADB_USER --password=$ACUMOS_MARIADB_USER_PASSWORD < $upgrade
+    else
+      fail "No available upgrade script for CDS upgrade from $ACUMOS_CDS_VERSION to $ACUMOS_CDS_PREVIOUS_VERSION"
+    fi
+  else
+    log "Redeploying with existing database versikon - no action required."
+  fi
+}
+
+source $AIO_ROOT/acumos-env.sh
+source $AIO_ROOT/utils.sh
+setup
