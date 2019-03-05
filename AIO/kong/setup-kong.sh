cbfea11ec2460e463d4b8166379e170d77ad9540
Bryan Sullivan <bryan.sullivan@att.com>
2019-03-04 22:01:39 -0800

    AIO upgrade to CDS 2.0
    Issue-ID: ACUMOS-2601, ACUMOS-2587
    
    Refactor into prereqs script (for admin)
    and deploy script for user (non-sudo).
    Add prep/deploy wrapper scripts for admin and normal user.
    Add Jupyter, Zeppelin, and NiFi baseline deploy.
    Deploy MariaDB and Elk via Helm.
    Reduce use of nodeports; route external access thru kong if possible
    Address public cloud use case (hostname different from domain name)
    Update user guide
    Add acumos_auth.py as 1st pass on user-level auth for docker-proxy
    Add docker-proxy README.md
    Add kong-configure job to secure kong admin setup
    Refocus peer-test.sh to peer relationship/subscription role
    Add add-host-alias.sh to update federation etc hosts aliases
    
    Change-Id: I4f88c0c258b66435a88c40a648bf88707a94e1e7
    Signed-off-by: Bryan Sullivan <bryan.sullivan@att.com>

--- AIO/kong/setup-kong.sh
+++ AIO/kong/setup-kong.sh
@@ -26,105 +26,69 @@
 #.
 
 function clean() {
+  trap 'fail' ERR
   if [[ "$DEPLOYED_UNDER" == "docker" ]]; then
     log "Stop any existing docker based components for kong-service"
-  #    sudo bash docker-compose.sh $AIO_ROOT down
-    cs=$(sudo docker ps -a | awk '/kong/{print $1}')
+    cs=$(docker ps -a | awk '/kong/{print $1}')
     for c in $cs; do
-      sudo docker stop $c
-      sudo docker rm $c
+      docker stop $c
+      docker rm $c
     done
   else
+    log "Remove job kong-configure if running"
+    stop_job kong-configure
     log "Stop any existing k8s based components for kong-service"
+    if [[ ! -e deploy/kong-service.yaml ]]; then
+      mkdir -p deploy
+      cp -r kubernetes/* deploy/.
+      replace_env deploy
+    fi
+    stop_service deploy/kong-admin-service.yaml
     stop_service deploy/kong-service.yaml
     stop_deployment deploy/kong-deployment.yaml
     log "Remove PVC for kong-service"
-    source $AIO_ROOT/setup-pv.sh clean pvc kong-db $ACUMOS_NAMESPACE
+    delete_pvc kong-db $ACUMOS_NAMESPACE
+    log "Remove configmap kong-config"
+    if [[ $($k8s_cmd get configmap -n $ACUMOS_NAMESPACE kong-config) ]]; then
+      $k8s_cmd delete configmap -n $ACUMOS_NAMESPACE kong-config
+    fi
   fi
-
-  log "Remove PV data for kong-service"
-  source $AIO_ROOT/setup-pv.sh clean pv kong-db $ACUMOS_NAMESPACE
 }
 
 function setup() {
   trap 'fail' ERR
-  if [[ "$ACUMOS_CDS_PREVIOUS_VERSION" == "" ]]; then clean; fi
 
-  log "Setup the kong-db PV"
-  source $AIO_ROOT/setup-pv.sh setup pv kong-db \
-    $ACUMOS_NAMESPACE $KONG_DB_PV_SIZE "$ACUMOS_HOST_USER:$ACUMOS_HOST_USER"
+  log "Add the Acumos cert and key to the configuration data"
+  cp ../certs/$ACUMOS_CERT config/.
+  cp ../certs/$ACUMOS_CERT_KEY config/.
 
-  log "Start kong-service components"
   if [[ "$DEPLOYED_UNDER" == "docker" ]]; then
-    sudo bash docker-compose.sh $AIO_ROOT up -d --build --force-recreate
+    log "Build the local configure-kong image"
+    docker build -t kong-configure .
+    log "Deploy the docker based components for kong"
+    source docker-compose.sh up -d --build --force-recreate
+    wait_running kong-service
   else
     log "Setup the kong-db PVC"
-    source $AIO_ROOT/setup-pv.sh setup pvc kong-db \
-      $ACUMOS_NAMESPACE $KONG_DB_PV_SIZE "$ACUMOS_HOST_USER:$ACUMOS_HOST_USER"
+    setup_pvc kong-db $ACUMOS_NAMESPACE $KONG_DB_PV_SIZE
 
     log "Deploy the k8s based components for kong"
     mkdir -p deploy
     cp -r kubernetes/* deploy/.
     replace_env deploy
+    start_service deploy/kong-admin-service.yaml
     start_service deploy/kong-service.yaml
     start_deployment deploy/kong-deployment.yaml
-  fi
+    wait_running kong $ACUMOS_NAMESPACE
 
-  wait_running kong
+    log "Create the kong-config configmap"
+    $k8s_cmd create configmap -n $ACUMOS_NAMESPACE kong-config --from-file=config
 
-  log "Verify kong admin API is ready"
-  url=http://$ACUMOS_KONG_ADMIN_HOST:$ACUMOS_KONG_ADMIN_PORT/apis
-  while ! curl $url ; do
-    log "Kong admin API is not responding... waiting 10 seconds"
-    sleep 10
-  done
-  until [[ $(curl $url | jq -r '.total') -ge 0 ]]; do
-    log "Kong admin API is not ready... waiting 10 seconds"
-    sleep 10
-  done
-
-  log "Pass cert and key to Kong admin"
-  curl -i -X POST http://$ACUMOS_KONG_ADMIN_HOST:$ACUMOS_KONG_ADMIN_PORT/certificates \
-    -F "cert=@$AIO_ROOT/certs/$ACUMOS_CERT" \
-    -F "key=@$AIO_ROOT/certs/$ACUMOS_CERT_KEY" \
-    -F "snis=$ACUMOS_DOMAIN"
-
-  log "Add proxy entries via Kong API"
-  curl -i -X POST \
-    --url http://$ACUMOS_KONG_ADMIN_HOST:$ACUMOS_KONG_ADMIN_PORT/apis/ \
-    --data "https_only=true" \
-    --data "name=root" \
-    --data "upstream_url=http://portal-fe-service:$ACUMOS_PORTAL_FE_PORT" \
-    --data "uris=/" \
-    --data "strip_uri=false" \
-    --data "upstream_connect_timeout=60000" \
-    --data "upstream_read_timeout=60000" \
-    --data "upstream_send_timeout=60000" \
-    --data "retries=5"
-
-  curl -i -X POST \
-    --url http://$ACUMOS_KONG_ADMIN_HOST:$ACUMOS_KONG_ADMIN_PORT/apis/ \
-    --data "https_only=true" \
-    --data "name=onboarding-app" \
-    --data "upstream_url=http://onboarding-service:$ACUMOS_ONBOARDING_PORT" \
-    --data "uris=/onboarding-app" \
-    --data "strip_uri=false" \
-    --data "upstream_connect_timeout=60000" \
-    --data "upstream_read_timeout=600000" \
-    --data "upstream_send_timeout=600000" \
-    --data "retries=5"
-
-  log "Dump of API endpoints as created"
-  curl http://$ACUMOS_KONG_ADMIN_HOST:$ACUMOS_KONG_ADMIN_PORT/apis/
-
-  if [[ "$DEPLOYED_UNDER" == "docker" ]]; then
-    log "Add cert as CA to docker /etc/docker/certs.d"
-    # TODO: Revisit need for this workaround in docker-dind based design
-    # Required for docker daemon to accept the kong self-signed cert
-    # Per https://docs.docker.com/registry/insecure/#use-self-signed-certificates
-    sudo mkdir -p /etc/docker/certs.d/$ACUMOS_HOST
-    sudo cp $AIO_ROOT/certs/$ACUMOS_CA_CERT /etc/docker/certs.d/$ACUMOS_HOST/ca.crt
+    log "Create the kong-configure job"
+    $k8s_cmd create -f deploy/kong-configure-job.yaml
+    wait_completed kong-configure
   fi
 }
 
+clean
 setup
