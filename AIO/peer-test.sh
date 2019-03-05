4e0c1d3f95293ebb30f583b664a84bab1ae30c56
Bryan Sullivan <bryan.sullivan@att.com>
2019-03-04 22:01:39 -0800

    AIO upgrade to CDS 2.0
    Issue-ID: ACUMOS-2601, ACUMOS-2587
    
    Refactor into prereqs script (for admin)
    and deploy script for user (non-sudo).
    Add prep/deploy wrapper scripts for admin and normal user.
    Add Jupyter, Zeppelin, and NiFi baseline deploy.
    Deploy MariaDB and Elk via Helm.
    Reduce use of nodeports; route external access thru kong if possible
    Address public cloud use case (hostname different from domain name)
    Update user guide
    Add acumos_auth.py as 1st pass on user-level auth for docker-proxy
    Add docker-proxy README.md
    Add kong-configure job to secure kong admin setup
    Refocus peer-test.sh to peer relationship/subscription role
    Add add-host-alias.sh to update federation etc hosts aliases
    
    Change-Id: I4f88c0c258b66435a88c40a648bf88707a94e1e7
    Signed-off-by: Bryan Sullivan <bryan.sullivan@att.com>

--- AIO/peer-test.sh
+++ AIO/peer-test.sh
@@ -23,34 +23,25 @@
 #   from host1 to host2.
 #
 # Prerequisites:
-# - Acumos deployed via oneclick_deploy.sh, and acumos-env.sh as updated by it
-# - key-based SSH access setup for running commands on the two target AIO hosts
-# - hostname of host1 and host2 resolvable in DNS or setup in /etc/hosts
-# - jq installed on the host
-# - For deployment of test models per the [models] option, a python localindex
-#   may need to be provided if the models are not packagable into containers
-#   by docker based upon the public pypi index. This local index is configured
-#   as per PYTHON_EXTRAINDEX and PYTHON_EXTRAINDEX_HOST in acumos-env.sh, and
-#   is served by twistd as setup in oneclick_deploy.sh
+# - Two Acumos AIO platforms deployed, with access to the saved environment files
+# - Both platforms deployed with certs from the same test CA, or a commercial CA
+# - All hostnames/FQDNs specified for peers must be DNS-resolvable on all hosts
+#   (entries in /etc/hosts or in an actual DNS server)
+# - jq installed on the host where this script is being run
 #
 # Usage:
-#   To deploy two Acumos platforms:
-#   $ bash peer-test.sh deploy <host1> <user1> <under1> <k8sdist1>
-#                              <host2> <user2> <under2> <k8sdist2>
-#   host1: AIO deploy target hostname
-#   user1: user account on host1
-#   under1: docker|k8s
-#   k8sdist1: generic|openshift
-#   host2: AIO deploy target hostname
-#   user2: user account on host2
-#   under2: docker|k8s
-#   k8sdist3: generic|openshift
+#   To peer and test two Acumos platforms:
+#   $ bash peer-test.sh <env1> <env2>
+#   env1: Path to platform 1 acumos-env.sh
+#   env2: Path to platform 2 acumos-env.sh
 #
-#   To setup two existing Acumos platforms (e.g. as deployed above) as peers:
-#   $ bash peer-test.sh peer <host1> <user1> <host2> <user2>
 
 function fail() {
-  log "$1"
+  set +x
+  trap - ERR
+  reason="$1"
+  if [[ "$1" == "" ]]; then reason="unknown failure at $fname $fline"; fi
+  log "$reason"
   exit 1
 }
 
@@ -62,102 +53,31 @@ function log() {
   set -x
 }
 
-function clean() {
+function peer_test() {
   trap 'fail' ERR
-  log "Cleaning Acumos AIO at $1 user $2"
-  # Copy latest clean.sh over in case some earlier bug in cleanup needs fixing...
-  ssh -x -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $2@$1 \
-    mkdir -p /home/$2/AIO
-  scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no clean.sh \
-    $2@$1:/home/$2/AIO/.
-  ssh -x -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $2@$1 <<EOF
-set -x
-trap 'exit 1' ERR
-cd AIO
-bash clean.sh
-rm -rf *
-EOF
-}
-
-function deploy() {
-  trap 'fail' ERR
-  log "Deploying Acumos AIO to $1 user $2"
-  scp -r -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
-    ../AIO $2@$1:/home/$2/.
-  # Run the commands separately to ensure failures are trapped
-  ssh -x -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $2@$1 <<EOF
-set -x
-trap 'exit 1' ERR
-cd AIO
-bash oneclick_deploy.sh $3 $4
-bash create-user.sh test P@ssw0rd test user test@acumos-aio.com Admin
-bash create-user.sh test P@ssw0rd test user test@acumos-aio.com Publisher
-EOF
-  scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
-     $2@$1:/home/$2/AIO/acumos-env.sh $1-env.sh
-  sed -i -- "s/ACUMOS_DOMAIN=.*/ACUMOS_DOMAIN=$1/" $1-env.sh
-}
-
-function verify_federation_api_access() {
-  log "Verify federation API access at $3 for $1"
-  source $1-env.sh
-  ssh -x -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $2@$1 \
-    curl -vk -o json \
-    --cert /var/$ACUMOS_NAMESPACE/certs/$ACUMOS_CERT \
-    --key /var/$ACUMOS_NAMESPACE/certs/$ACUMOS_CERT_KEY \
-    https://$3:$ACUMOS_FEDERATION_PORT/solutions
-  scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
-    $2@$1:/home/$2/json json
-  cat json
-  err=$(jq -r '.error' json)
-  if [[ "$err" != "null" ]]; then
-    fail "Solution retrieval failed"
-  fi
-}
-
-function create_peers() {
-  trap 'fail' ERR
-  log "Create $host2 peer at $host1"
-  source $host1-env.sh
-  source create-peer.sh $host1 $user1 ${host2}CA.crt $host2 admin@example.com
-  log "Create $host1 peer at $host2"
-  source $host2-env.sh
-  source create-peer.sh $host2 $user2 ${host1}CA.crt $host1 admin@example.com
-
-  verify_federation_api_access $host1 $user1 $host2
-  verify_federation_api_access $host2 $user2 $host1
+  set +x
+  source $env1
+  set -x
+  name1=$ACUMOS_DOMAIN
+  peergw1="https://$ACUMOS_DOMAIN:$ACUMOS_FEDERATION_PORT"
+  set +x
+  source $env2
+  set -x
+  name2=$ACUMOS_DOMAIN
+  peergw2="https://$ACUMOS_DOMAIN:$ACUMOS_FEDERATION_PORT"
+  bash create-peer.sh $env1 $name2 "admin@$name2" "$peergw2"
+  bash create-peer.sh $env2 $name1 "admin@$name1" "$peergw1"
+  bash create-subscription.sh $env1 test $name2 PB FL 1 "CL,DS,DT,PR,RG"
+  bash create-subscription.sh $env2 test $name1 PB FL 1 "CL,DS,DT,PR,RG"
 }
 
 set -x
 trap 'fail' ERR
-export WORK_DIR=$(pwd)
-if [[ "$1" == "deploy" ]]; then
-  host1=$2
-  user1=$3
-  under1=$4
-  k8sdist1=$5
-  host2=$6
-  user2=$7
-  under2=$8
-  k8sdist2=$9
-  clean $host1 $user1
-  deploy $host1 $user1 $under1 $k8sdist1
-  clean $host2 $user2
-  deploy $host2 $user2 $under2 $k8sdist2
-  echo <<EOF
-Deployment is complete at $host1 and $host2. To complete the test run
-bash peer-test.sh peer $host1 $user1 $host2 $user2
+WORK_DIR=$(pwd)
+env1=$1
+env2=$2
+peer_test
+echo <<EOF
+Peer relationship and subscriptions are setup. Any public marketplace models
+should appear in the peer in about a minute.
 EOF
-else
-  host1=$2
-  user1=$3
-  host2=$4
-  user2=$5
-  create_peers
-  echo <<EOF
-Peering is complete at $host1 and $host2. To complete the test,
-1) login to $host1 as user 'test' with password 'P@ssw0rd!', and manually
-   complete the model publication process to the Public Marketplace
-2) Repeat (1) for $host2
-EOF
-fi
