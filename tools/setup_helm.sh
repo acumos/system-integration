5f55a4d93eb854b84793a7863c7935aa7b05f4a2
Bryan Sullivan <bryan.sullivan@att.com>
2019-04-05 12:08:51 -0700

    Update to weekly assembly Acumos_1903232099
    Issue-ID: ACUMOS-2714, ACUMOS-2715, ACUMOS-2716, ACUMOS-2717
    Issue-ID: ACUMOS-2718, ACUMOS-2721
    
    Update to release assembly Acumos_1903232100
    Add input parameter check and usage help to scripts
    Align script naming convention
    Integrate cleanup steps in acumos_k8s_prep.sh
    Enable component scripts to be run separately
    Add option for docker-on-host to address Azure-k8s issues
    Add security-verification service component
    Support Helm use in Openshift deployments
    Add scripts enabling redeployment of specific components
    Add Nifi Registry service deployment
    Add NiFi User service deployment
    Remove Hippo CMS
    
    Change-Id: I2ff2e74ebb6177280e1737f4bc6b8ad98174088d
    Signed-off-by: Bryan Sullivan <bryan.sullivan@att.com>

--- tools/setup_helm.sh
+++ tools/setup_helm.sh
@@ -17,16 +17,17 @@
 # limitations under the License.
 # ===============LICENSE_END=========================================================
 #
-#. What this is: script to setup Helm as kubernetes chart manager
-#.
-#. Prerequisites:
-#. - Kubernetes cluster deployed, e.g. via setup_k8s.sh
-#.
-#. Usage:
-#. $ bash setup_helm.sh
-#.
-
-trap 'fail' ERR
+# What this is: script to setup Helm as kubernetes chart manager
+#
+# Prerequisites:
+# - Linux (Ubuntu Xenial/Bionic) host for the k8s cluster
+# - Kubernetes cluster deployed, e.g. via setup_k8s.sh or setup_openshift.sh
+# - If running on a remote workstation, access to the k8s cluster established
+#   e.g. via setup_kubectl.sh or setup_openshift_client.sh
+#
+# Usage:
+# $ bash setup_helm.sh
+#
 
 function fail() {
   log "$1"
@@ -40,38 +41,83 @@ function log() {
 }
 
 function setup_helm() {
+  trap 'fail' ERR
   log "Setup helm"
   # Install Helm
-  # per https://github.com/kubernetes/helm/blob/master/docs/install.md
-  cd ~
-  curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
-  chmod 700 get_helm.sh
-  ./get_helm.sh
-  log "Initialize helm"
-  helm init
-#  nohup helm serve > /dev/null 2>&1 &
-#  log "Run helm repo update"
-#  helm repo update
-  # TODO: Workaround for bug https://github.com/kubernetes/helm/issues/2224
-  # For testing use only!
-  kubectl create clusterrolebinding permissive-binding \
-    --clusterrole=cluster-admin --user=admin --user=kubelet \
-    --group=system:serviceaccounts;
-  # TODO: workaround for tiller FailedScheduling (No nodes are available that 
+  if [[ $(which oc) ]]; then
+    # Per https://blog.openshift.com/getting-started-helm-openshift/
+    log "Install the Helm client locally"
+    curl -s https://storage.googleapis.com/kubernetes-helm/helm-v2.12.3-linux-amd64.tar.gz | tar xz
+    cd linux-amd64
+    sudo cp helm /usr/bin/helm
+    helm init --client-only
+    export TILLER_NAMESPACE=kube-system
+    oc project kube-system
+    log "Install the tiller service on the openshift cluster"
+    oc process \
+     -f https://github.com/openshift/origin/raw/master/examples/helm/tiller-template.yaml\
+     -p TILLER_NAMESPACE="${TILLER_NAMESPACE}" \
+     -p HELM_VERSION=v2.12.3 | oc create -f -
+    log "Grant tiller the RBAC permission to act as a cluster-admin service account"
+    kubectl create clusterrolebinding tiller-binding \
+      --clusterrole=cluster-admin --serviceaccount ${TILLER_NAMESPACE}:tiller
+#    oc policy add-role-to-user admin "system:serviceaccount:${TILLER_NAMESPACE}:tiller"
+  else
+    # per https://github.com/kubernetes/helm/blob/master/docs/install.md
+    cd ~
+    curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
+    chmod 700 get_helm.sh
+    ./get_helm.sh
+    log "Initialize helm"
+    helm init
+  #  nohup helm serve > /dev/null 2>&1 &
+  #  log "Run helm repo update"
+  #  helm repo update
+    # TODO: Workaround for bug https://github.com/kubernetes/helm/issues/2224
+    # For testing use only!
+    kubectl create clusterrolebinding permissive-binding \
+      --clusterrole=cluster-admin --user=admin --user=kubelet \
+      --group=system:serviceaccounts;
+  fi
+
+  # TODO: workaround for tiller FailedScheduling (No nodes are available that
   # match all of the following predicates:: PodToleratesNodeTaints (1).)
   # kubectl taint nodes $HOSTNAME node-role.kubernetes.io/master:NoSchedule-
   # Wait till tiller is running
-  tiller_deploy=$(kubectl get pods --all-namespaces | grep tiller-deploy | awk '{print $4}')
-  while [[ "$tiller_deploy" != "Running" ]]; do
-    log "tiller-deploy status is $tiller_deploy. Waiting 60 seconds for it to be 'Running'"
-    sleep 60
-    tiller_deploy=$(kubectl get pods --all-namespaces | grep tiller-deploy | awk '{print $4}')
+  log "Waiting for tiller pod to be 'Running'"
+  status="/tmp/$(uuidgen)"
+  pod=$(kubectl get pods -n kube-system | awk '/tiller\-/{print $1}')
+  kubectl get pods -o json -n kube-system $pod >$status
+  i=0
+  while [[ "$(jq -r '.status.phase' $status)" != "Running" ]]; do
+    ((++i))
+    if [[ $i -eq 60 ]]; then
+      fail "tiller status is $(jq -r '.status.phase' $status) after 10 minutes"
+      rm $status
+    fi
+    kubectl get pods -n kube-system | awk '/tiller\-/{print $1}'
+    log "tiller status is $(jq -r '.status.phase' $status). Waiting 10 seconds..."
+    sleep 10
+    kubectl get pods -o json -n kube-system $pod >$status
   done
-  log "tiller-deploy status is $tiller_deploy"
+  log "tiller-deploy status is $(jq -r '.status.phase' $status)"
+  rm $status
+
+  log "Waiting until helm responds to clients"
+  i=0
+  while ! helm list ; do
+    ((++i))
+    if [[ $i -eq 30 ]]; then
+      fail "Helm is not responding after 5 minutes"
+    fi
+    log "Helm server is not yet responding, waiting 10 seconds"
+    sleep 10
+  done
 
   # Install services via helm charts from https://kubeapps.com/charts
   # e.g. helm install stable/dokuwiki
 }
 
+trap 'fail' ERR
 setup_helm
 log "Setup is complete."
