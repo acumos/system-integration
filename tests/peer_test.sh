91a6fd8a87107cf305f6b3e40a460ca9aee076a3
Bryan Sullivan <bryan.sullivan@att.com>
2019-04-05 12:08:51 -0700

    Update to weekly assembly Acumos_1903232099
    Issue-ID: ACUMOS-2714, ACUMOS-2715, ACUMOS-2716, ACUMOS-2717
    Issue-ID: ACUMOS-2718, ACUMOS-2721
    
    Update to release assembly Acumos_1903232100
    Add input parameter check and usage help to scripts
    Align script naming convention
    Integrate cleanup steps in acumos_k8s_prep.sh
    Enable component scripts to be run separately
    Add option for docker-on-host to address Azure-k8s issues
    Add security-verification service component
    Support Helm use in Openshift deployments
    Add scripts enabling redeployment of specific components
    
    Change-Id: I2ff2e74ebb6177280e1737f4bc6b8ad98174088d
    Signed-off-by: Bryan Sullivan <bryan.sullivan@att.com>

---
+++ tests/peer_test.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+# ===============LICENSE_START=======================================================
+# Acumos Apache-2.0
+# ===================================================================================
+# Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+# ===================================================================================
+# This Acumos software file is distributed by AT&T and Tech Mahindra
+# under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# This file is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============LICENSE_END=========================================================
+#
+# What this is: test script for deploying two Acumos AIO instances and
+#   verifying federation. Deploys Acumos in AIO configuration, uploads models
+#   to one AIO instance, federates the two instances, and verifies model sync
+#   from host1 to host2.
+#
+# Prerequisites:
+# - Two Acumos AIO platforms deployed, with access to the saved environment files
+# - Both platforms deployed with certs from the same test CA, or a commercial CA
+# - All hostnames/FQDNs specified for peers must be DNS-resolvable on all hosts
+#   (entries in /etc/hosts or in an actual DNS server)
+# - jq installed on the host where this script is being run
+#
+#. Usage:
+#.   To peer and test two Acumos platforms:
+#.   $ bash peer_test.sh <env1> <admin1> <env2> <admin2>
+#.   env1: Path to platform 1 acumos_env.sh
+#.   admin1: Userid of Admin role user on platform 1
+#.   env2: Path to platform 2 acumos_env.sh
+#.   admin2: Userid of Admin role user on platform 2
+#
+
+function fail() {
+  set +x
+  trap - ERR
+  reason="$1"
+  if [[ "$1" == "" ]]; then reason="unknown failure at $fname $fline"; fi
+  log "$reason"
+  exit 1
+}
+
+function log() {
+  set +x
+  fname=$(caller 0 | awk '{print $2}')
+  fline=$(caller 0 | awk '{print $1}')
+  echo; echo "$fname:$fline ($(date)) $1"
+  set -x
+}
+
+function peer_test() {
+  trap 'fail' ERR
+  set +x
+  source $env1
+  set -x
+  name1=$ACUMOS_DOMAIN
+  peergw1="https://$ACUMOS_DOMAIN:$ACUMOS_FEDERATION_PORT"
+  set +x
+  source $env2
+  set -x
+  name2=$ACUMOS_DOMAIN
+  peergw2="https://$ACUMOS_DOMAIN:$ACUMOS_FEDERATION_PORT"
+  bash create_peer.sh $env1 $name2 "admin@$name2" "$peergw2"
+  bash create_peer.sh $env2 $name1 "admin@$name1" "$peergw1"
+  bash create_subscription.sh $env1 $admin1 $name2 PB FL 1 "CL,DS,DT,PR,RG"
+  bash create_subscription.sh $env2 $admin2 $name1 PB FL 1 "CL,DS,DT,PR,RG"
+}
+
+set -x
+trap 'fail' ERR
+
+if [[ $# -ge 6 ]]; then
+  env1=$1
+  admin1=$2
+  env2=$3
+  admin2=$4
+  WORK_DIR=$(pwd)
+  cd $(dirname "$0")
+  peer_test
+  echo <<EOF
+Peer relationship and subscriptions are setup. Any public marketplace models
+should appear in the peer in about a minute.
+EOF
+else
+  grep '#. ' $0 | sedi 's/#.//g'
+fi
