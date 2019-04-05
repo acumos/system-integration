79bb207709c7c3c12cc3ea9ee8f263c908617c31
Bryan Sullivan <bryan.sullivan@att.com>
2019-04-05 12:08:51 -0700

    Update to weekly assembly Acumos_1904021700
    Issue-ID: ACUMOS-2714, ACUMOS-2715, ACUMOS-2716, ACUMOS-2717
    Issue-ID: ACUMOS-2718, ACUMOS-2721
    
    Update to release assembly Acumos_1903232100
    Add input parameter check and usage help to scripts
    Align script naming convention
    Integrate cleanup steps in acumos_k8s_prep.sh
    Enable component scripts to be run separately
    Add option for docker-on-host to address Azure-k8s issues
    Add security-verification service component
    Support Helm use in Openshift deployments
    Add scripts enabling redeployment of specific components
    Add Nifi Registry service deployment
    Add NiFi User service deployment
    Remove Hippo CMS
    
    Change-Id: I2ff2e74ebb6177280e1737f4bc6b8ad98174088d
    Signed-off-by: Bryan Sullivan <bryan.sullivan@att.com>

---
+++ tests/onboard_model.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+# ===============LICENSE_START=======================================================
+# Acumos Apache-2.0
+# ===================================================================================
+# Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+# ===================================================================================
+# This Acumos software file is distributed by AT&T and Tech Mahindra
+# under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# This file is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============LICENSE_END=========================================================
+#
+#. What this is: script to automate model onboarding.
+#.
+#. Prerequisites:
+#. - User has account on Acumos platform
+#. - Model dumped by the Acumos client library, i.e. the following files
+#.   - metadata.json
+#.   - model.proto
+#.   - model.zip
+#.
+#. Usage:
+#. $ bash onboard-model.sh <host> <username> <password> <model> <insecure>
+#.   host: host of the model onboarding service, including port if needed
+#.   username: username to onboard models for
+#.   password: password for user
+#.   model: folder with model to onboard
+#.   insecure: optional flag allowing onboarding to insecure server (installed
+#.      with self-signed server cert, as needed for test platforms)
+
+trap 'fail' ERR
+
+function fail() {
+  log "$1"
+  cd $WORK_DIR
+  rm $json
+  exit 1
+}
+
+function log() {
+  f=$(caller 0 | awk '{print $2}')
+  l=$(caller 0 | awk '{print $1}')
+  echo; echo "$f:$l ($(date)) $1"
+}
+
+function onboard() {
+  trap 'fail' ERR
+  AUTHURL=https://$host/onboarding-app/v2/auth
+  PUSHURL=https://$host/onboarding-app/v2/models
+
+  json="/tmp/$(date +%H%M%S%N)"
+
+  log "Query rest service at host $host to get token"
+  curl -o $json -k -X POST -H 'Content-Type: application/json' -H 'Accept: application/json' $AUTHURL -d "{\"request_body\":{\"username\":\"$user\",\"password\":\"$pass\"}}"
+  if [[ $(grep -c "doctype html" $json) -gt 0 ]]; then
+    cat $json
+    fail "Authentication failed at host $host"
+  fi
+  jwtToken=$(jq -r '.jwtToken' $json)
+  if [[ "$jwtToken" == "null" ]]; then
+    cat $json
+    fail "Authentication failed at host $host"
+  fi
+
+  # Use the jwtToken for onboarding
+  log "Authentication successful"
+  echo "Onboarding model $model at $host ..."
+  set -x
+  proto=$(ls $model/*.proto)
+  if [[ "$insecure" == "insecure" ]]; then k="-k"; fi
+  if [[ -e $model/license.json ]]; then
+    log "Onboarding with license file license.json"
+    curl -o $json $k -H "Authorization: $jwtToken" \
+         -F "license=@$model/license.json;type=application/json" \
+         -F "model=@$model/model.zip;type=application/zip" \
+         -F "metadata=@$model/metadata.json;type=application/json" \
+         -F "schema=@$proto;type=application/text" $PUSHURL
+  else
+    curl -o $json $k -H "Authorization: $jwtToken" \
+         -F "model=@$model/model.zip;type=application/zip" \
+         -F "metadata=@$model/metadata.json;type=application/json" \
+         -F "schema=@$proto;type=application/text" $PUSHURL
+  fi
+  set +x
+  if [[ $(grep -c -e "The upstream server is timing out" -e "Service unavailable" $json) -gt 0 ]]; then
+    log "Onboarding $model failed at host $host"
+    cat $json
+  else
+    status=$(jq -r '.status' $json)
+    if [[ "$status" != "ERROR" ]]; then
+      log "Onboarding $model succeeded at host $host"
+    else
+      log "Onboarding $model failed at host $host"
+      cat $json
+    fi
+  fi
+}
+
+if [[ "$#" -lt 4 ]]; then
+  echo "All required parameters not provided"
+  if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then grep '#\.' $0; fi
+else
+  host=$1
+  user=$2
+  pass=$3
+  model="$4"
+  insecure=$5
+  onboard
+fi
---
+++ tools/setup_docker.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+# ===============LICENSE_START=======================================================
+# Acumos Apache-2.0
+# ===================================================================================
+# Copyright (C) 2017-2018 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+# ===================================================================================
+# This Acumos software file is distributed by AT&T and Tech Mahindra
+# under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# This file is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============LICENSE_END=========================================================
+#
+# What this is: Script to install docker and docker-compose on a host, if needed
+#
+# Prerequisites:
+# - Ubuntu Xenial or Centos 7 server
+#
+# Usage:
+# - bash setup_docker.sh
+#
+
+set -x
+
+function fail() {
+  log "$1"
+  exit 1
+}
+
+trap 'fail' ERR
+
+function log() {
+  set +x
+  fname=$(caller 0 | awk '{print $2}')
+  fline=$(caller 0 | awk '{print $1}')
+  echo; echo "$fname:$fline ($(date)) $1"
+  set -x
+}
+
+function wait_dpkg() {
+  # TODO: workaround for "E: Could not get lock /var/lib/dpkg/lock - open (11: Resource temporarily unavailable)"
+  echo; echo "waiting for dpkg to be unlocked"
+  while sudo fuser /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
+    sleep 1
+  done
+}
+
+setup() {
+  if [[ "$HOST_OS" == "ubuntu" ]]; then
+    # Per https://kubernetes.io/docs/setup/independent/install-kubeadm/
+    log "Install latest docker.ce"
+    # Per https://docs.docker.com/install/linux/docker-ce/ubuntu/
+    wait_dpkg
+    if [[ $(sudo apt-get purge -y docker-ce docker docker-engine docker.io) ]]; then
+      echo "Purged docker-ce docker docker-engine docker.io"
+    fi
+    wait_dpkg; sudo apt-get update
+    wait_dpkg; sudo apt-get install -y \
+      apt-transport-https \
+      ca-certificates \
+      curl \
+      software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    wait_dpkg; sudo apt-get update
+    apt-cache madison docker-ce
+    wait_dpkg; sudo apt-get install -y docker-ce=18.06.3~ce~3-0~ubuntu
+  elif [[ "$HOST_OS" == "centos" ]]; then
+    log "Install latest docker-ce in Centos"
+    # per https://docs.docker.com/engine/installation/linux/docker-ce/centos/#install-from-a-package
+    sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+    sudo yum-config-manager --add-repo \
+      https://download.docker.com/linux/centos/docker-ce.repo
+    sudo yum install -y docker-ce
+    sudo systemctl enable docker
+    sudo systemctl start docker
+  else
+    fail "Unsupported host OS: $HOST_OS"
+  fi
+
+  log "Install latest docker-compose v3.2"
+  # Required, to use docker compose version 3.2 templates
+  # Per https://docs.docker.com/compose/install/#install-compose
+  # Current version is listed at https://github.com/docker/compose/releases
+  sudo curl -L -o /usr/local/bin/docker-compose \
+    "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)"
+  sudo chmod +x /usr/local/bin/docker-compose
+}
+
+export HOST_OS=$(grep --m 1 ID /etc/os-release | awk -F '=' '{print $2}' | sed 's/"//g')
+export HOST_OS_VER=$(grep -m 1 'VERSION_ID=' /etc/os-release | awk -F '=' '{print $2}' | sed 's/"//g')
+setup
