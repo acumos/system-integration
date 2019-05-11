156b581ede5c68cbc467bcb2eceab2c114c06e76
Bryan Sullivan <bryan.sullivan@att.com>
2019-05-10 18:31:53 -0700

    Integrate MLWB components
    Issue-ID: ACUMOS-2194
    
    Change-Id: I773f2ef50acf551a5e14b584a233282091457203
    Signed-off-by: Bryan Sullivan <bryan.sullivan@att.com>

--- tools/setup_kubectl.sh
+++ tools/setup_kubectl.sh
@@ -35,8 +35,6 @@
 #.   namespace: namespace to use
 #.
 
-trap 'fail' ERR
-
 function fail() {
   log "$1"
   exit 1
@@ -129,17 +127,23 @@ APISERVER=$(ssh -x -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
 if [[ "$APISERVER" == "" ]]; then
   fail "Unable to retrieve API server URL"
 fi
+log "APISERVER=$APISERVER"
 SECRET=$(ssh -x -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $user@$server \
   kubectl get secrets | grep -m1 ^default-token | cut -f1 -d ' ')
 if [[ "$SECRET" == "" ]]; then
   fail "Unable to retrieve default-token secret"
 fi
+log "SECRET=$SECRET"
 TOKEN=$(ssh -x -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $user@$server \
   kubectl describe secret $SECRET | grep -E '^token' | cut -f2 -d':' | tr -d " ")
 if [[ "$TOKEN" == "" ]]; then
   fail "Unable to retrieve token"
 fi
+log "TOKEN=$TOKEN"
 curl $APISERVER/api --header "Authorization: Bearer $TOKEN" --insecure
+
+kubectl config delete-cluster $server
+kubectl config delete-context $server-$namespace
 
 kubectl config set-cluster $server --server=$APISERVER \
   --insecure-skip-tls-verify=true
@@ -148,10 +152,10 @@ kubectl config set-context $server-$namespace --cluster=$server \
 kubectl config set-credentials $user --token=$TOKEN
 kubectl config use-context $server-$namespace
 
-if [[ $(kubectl get pods --all-namespaces | grep -c kube-system) -gt 0 ]]; then
+if [[ $(kubectl get namespaces | grep -c kube-system) -gt 0 ]]; then
   log "Setup is complete!"
 else
-  fail "Setup failed - unable to retrieve pods"
+  fail "Setup failed"
 fi
 
 cd $WORK_DIR
