ef26e9a2014dcc5ba05ca809622736c930fe6ac7
Bryan Sullivan <bryan.sullivan@att.com>
2019-08-26 08:19:04 -0700

    Baseline of Jenkins and MLWB Model Service
    Issue-ID: ACUMOS-3134, ACUMOS-3177
    
    Signed-off-by: Bryan Sullivan <bryan.sullivan@att.com>
    Change-Id: I8fd36c3dfecedc491e19300b8c06664a7354f612

---
+++ charts/jenkins/setup_jenkins.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+# ===============LICENSE_START=======================================================
+# Acumos Apache-2.0
+# ===================================================================================
+# Copyright (C) 2019 AT&T Intellectual Property & Tech Mahindra. All rights reserved.
+# ===================================================================================
+# This Acumos software file is distributed by AT&T and Tech Mahindra
+# under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# This file is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============LICENSE_END=========================================================
+#
+# What this is: script to setup a Jenkins server under k8s with Nginx-based ingress.
+#
+# Prerequisites:
+# - k8s cluster deployed
+# - k8s ingress controller deployed at K8S_INGRESS_DOMAIN and secret
+#   'ingress-cert' created per charts/ingress/setup_ingress_controller.sh.
+#
+# Usage:
+# For k8s-based deployment, run this script on the k8s master or a workstation
+# connected to the k8s cluster via kubectl.
+# $ bash setup_jupyterhub.sh <setup|clean|all> <NAMESPACE> <K8S_INGRESS_ORIGIN>
+#   setup|clean|all: action to take
+#   K8S_INGRESS_DOMAIN: domain assigned to the k8s cluster ingress controller
+#   NAMESPACE: k8s namespace to deploy under (will be created if not existing)
+#
+
+function clean_jenkins() {
+  trap 'fail' ERR
+
+  if [[ $(helm delete --purge jenkins) ]]; then
+    log "Helm release jenkins deleted"
+  fi
+}
+
+function setup_jenkins() {
+  trap 'fail' ERR
+
+  log "Update values.yaml as input to Helm for deploying the Jenkins chart"
+  get_host_ip $K8S_INGRESS_DOMAIN
+  sedi "s/<NAMESPACE>/$NAMESPACE/g" values.yaml
+  sedi "s/<K8S_INGRESS_DOMAIN>/$K8S_INGRESS_DOMAIN/g" values.yaml
+  sedi "s/<HOST_IP>/$HOST_IP/g" values.yaml
+
+  log "Resulting values.yaml:"
+  cat values.yaml
+
+  log "Install jenkins via Helm"
+  helm install --name jenkins -f values.yaml stable/jenkins
+}
+
+if [[ $# -lt 3 ]]; then
+  cat <<'EOF'
+Usage:
+ For k8s-based deployment, run this script on the k8s master or a workstation
+ connected to the k8s cluster via kubectl.
+ $ bash setup_jupyterhub.sh <setup|clean|all> <NAMESPACE> <K8S_INGRESS_ORIGIN>
+   setup|clean|all: action to take
+   K8S_INGRESS_ORIGIN: origin (FQDN:port) assigned to the k8s cluster ingress controller
+   NAMESPACE: k8s namespace to deploy under (will be created if not existing)
+EOF
+  echo "All parameters not provided"
+  exit 1
+fi
+
+set -x
+trap 'fail' ERR
+WORK_DIR=$(pwd)
+cd $(dirname "$0")
+if [[ -z "$AIO_ROOT" ]]; then export AIO_ROOT="$(cd ../../AIO; pwd -P)"; fi
+source $AIO_ROOT/utils.sh
+
+action=$1
+NAMESPACE=$2
+K8S_INGRESS_DOMAIN=$3
+K8S_DIST=generic
+k8s_cmd=kubectl
+k8s_nstype=namespace
+if [[ "$action" == "clean" || "$action" == "all" ]]; then clean_jenkins; fi
+if [[ "$action" == "setup" || "$action" == "all" ]]; then setup_jenkins; fi
+cd $WORK_DIR
