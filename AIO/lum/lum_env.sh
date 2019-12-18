3ea398577aa0f85f99cd16c630a05e0ebbfb86de
paragraval23 <parag.raval@est.tech>
2019-12-18 09:52:39 -0800

    AIO > docker based deployment for LUM
    Change-Id: I45151c8a9c9e154c42fc8a14a8cf47be0316c79b
    Issue-ID: ACUMOS-3859
    Signed-off-by: paragraval23 <parag.raval@est.tech>

---
+++ AIO/lum/lum_env.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# ===============LICENSE_START=======================================================
+# Acumos Apache-2.0
+# ===================================================================================
+# Copyright (C) 2019 Nordix Foundation.
+# ===================================================================================
+# This Acumos software file is distributed by Nordix Foundation
+# under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# This file is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============LICENSE_END=========================================================
+#
+# What this is: Environment file for LUM installation.
+# Prerequisites:
+# - Ubuntu Xenial or Centos 7 server
+# Usage:
+# - Intended to be called from setup-lum.sh
+#
+
+export LUM_POSTGRES_USER=lumdb
+export LUM_POSTGRES_PASSWORD=lumdb
